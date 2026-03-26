5bdd39f1c2f28dbf4f5dddae3da2bd2cbb7eac58
Antonio Hernandez <antoniohl91@gmail.com>
2026-03-26 14:11:59 +0100

    Factory Method: Creamos producto abstracto

---
+++ src/patrones_creacionales/factory_method/Vehículo.java
@@ -0,0 +1,20 @@
+package patrones_creacionales.factory_method;
+
+/**
+ * PRODUCTO ABSTRACTO
+ *
+ * Con esta interfaz generamos el "contrato" que todas las subclases tienen que cumplir
+ * No importa el objecto que sea, en el ambito de 'Vehículos' podemos hablar de 'Coche',
+ * 'Moto', 'Quad'... todos deben saber hacer lo que se indica aquí.
+ *
+ * Usamos una interfaz, porque no nos interesa que el código que usa vehículos sepa, ni le importe,
+ * si es un coche, una moto, un avión... Sola sabe que es un 'Vehículo'
+ */
+public interface Vehículo {
+
+    // - Todos los vehículos sabrán arrancar, pero cada uno a su manera.
+    void arrancar();
+
+    // - Todos los vehículos sabrán dar información sobre ellos mismos
+    void describirse();
+}
